51aa1ab0bb8acb19c90b9db56d4fdbdf92330b80
Nabilleb <nabilgreen500@gmail.com>
2025-08-17 14:38:17 +0300

    check if email exists in login

--- login.php
+++ login.php
@@ -1,6 +1,16 @@
 <?php
 session_start();
 include "connect.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $email =$_POST['email'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT UserID, PasswordHash, FullName FROM Users WHERE Email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $stmt->store_result();
+}
 ?>
 
 <form method="post">
--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 session_start();
 include "connect.php";
 
-if ($_SERVER["REQUEST_METHOD"] === "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
 $email = $_POST['email'];
 $fullname = $_POST['fullname'];
 $password = $_POST['password'];
