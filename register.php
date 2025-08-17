63fb664d3626a3728feab42b282a2f6b52554d0e
Nabilleb <nabilgreen500@gmail.com>
2025-08-17 13:35:37 +0300

    checks if user exsits in register

--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include "connect.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST"){
 $email = $_POST['email'];
@@ -7,7 +8,10 @@ $fullname = $_POST['fullname'];
 $password = $_POST['password'];
 }
 
-?>
+ $check = $conn->prepare("SELECT UserID FROM Users WHERE Email = ?");
+    $check->bind_param("s", $email);
+    $check->execute();
+    $check->store_result();?>
 
 <form method="post">
     <h2>Register</h2>
