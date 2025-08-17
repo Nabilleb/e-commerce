d90dc66ab25c825bac044fb74a61cd0e192a7975
Nabilleb <nabilgreen500@gmail.com>
2025-08-17 13:24:42 +0300

    established a connection from backend

--- connect.php
+++ connect.php
@@ -9,6 +9,6 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "✅ Connected successfully";
+echo " Connected successfully";
 
 ?>
