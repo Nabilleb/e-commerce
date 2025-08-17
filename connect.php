73025ee28a6cf2574e6f026ce9cf41568ed0f9ca
Nabilleb <nabilgreen500@gmail.com>
2025-08-17 12:52:32 +0300

    first commit

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "e-com";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "✅ Connected successfully";
+
+?>
