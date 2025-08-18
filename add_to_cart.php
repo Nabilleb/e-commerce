027492bad6732b7b31bc7ecef54cbee1e00b609e
Nabilleb <nabilgreen500@gmail.com>
2025-08-18 12:52:47 +0300

    check if carts exsits

--- add_to_cart.php
+++ add_to_cart.php
@@ -10,4 +10,11 @@ if (!isset($_SESSION['UserID'])){
 $userID = $_SESSION['UserID'];
 $productID = intval($_GET['id']); 
 $quantity = isset($_GET['qty']) ? intval($_GET['qty']) : 1
+
+$cartCheck = $conn->prepare("SELECT CartID FROM Carts WHERE UserID = ?");
+$cartCheck->bind_param("i", $userID);
+$cartCheck->execute();
+$cartCheck->bind_result($cartID);
+$cartCheck->fetch();
+$cartCheck->close();
 ?>
