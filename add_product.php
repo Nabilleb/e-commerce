6b585335584163924b50fa7120052733cade405f
Nabilleb <nabilgreen500@gmail.com>
2025-08-18 15:26:30 +0300

    can add product

--- add_product.php
+++ add_product.php
@@ -8,7 +8,20 @@ if(!isset($_SESSION['UserID'])){
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+   $name = $_POST['name'];
+    $desc = $_POST['description'];
+    $price = $_POST['price'];
+    $stock = $_POST['stock'];
+    $category = $_POST['category'];
 
+    $stmt = $conn->prepare("INSERT INTO Products (CategoryID, ProductName, Description, Price, Stock) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("issdi", $category, $name, $desc, $price, $stock);
+
+    if ($stmt->execute()) {
+        echo "Product added! <a href='manage_products.php'>Manage Products</a>";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
 }
 
 ?>
