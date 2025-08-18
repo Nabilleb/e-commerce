cbe81bd43028aabb4e6a175e214667e26623c104
Nabilleb <nabilgreen500@gmail.com>
2025-08-18 12:40:54 +0300

    get id for a single product

--- product.php
+++ product.php
@@ -1,8 +1,13 @@
 <?php
 include "connect.php";
- if(!isset($_GET['id'])){
-    echo "product not found";
-    exit
- }
+if (!isset($_GET['id'])) {
+    echo "Product not found!";
+    exit;
+}
 
+$id = intval($_GET['id']);
+$stmt = $conn->prepare("SELECT * FROM Products WHERE ProductID = ? AND IsActive = 1");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$result = $stmt->get_result();
 ?>
