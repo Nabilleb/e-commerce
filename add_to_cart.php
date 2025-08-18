e10ede2906d28bbfe72f013c5deded95893b1c20
Nabilleb <nabilgreen500@gmail.com>
2025-08-18 12:49:26 +0300

    get user id and product id and qty

--- add_to_cart.php
+++ add_to_cart.php
@@ -6,4 +6,8 @@ if (!isset($_SESSION['UserID'])){
     header("Location: login.php");
     exit;
 }
+
+$userID = $_SESSION['UserID'];
+$productID = intval($_GET['id']); 
+$quantity = isset($_GET['qty']) ? intval($_GET['qty']) : 1
 ?>
