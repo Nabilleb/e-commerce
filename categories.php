eca6108438890d008ec513d647fc1a3816572e83
Nabilleb <nabilgreen500@gmail.com>
2025-08-18 11:47:07 +0300

    can get categories now from backend

--- categories.php
+++ categories.php
@@ -6,4 +6,12 @@ if(!isset($_SESSION['UserID'])){
       header("Location: login.php");
     exit;
 }
+
+$result = $conn->query("SELECT * FROM Categories");
+
+echo "<h2>Categories</h2>";
+while ($row = $result->fetch_assoc()) {
+    echo "<a href='products.php?category=" . $row['CategoryID'] . "'>" 
+         . htmlspecialchars($row['CategoryName']) . "</a><br>";
+}
 ?>
