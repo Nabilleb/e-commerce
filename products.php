e242ea55cacde4fb1ae0c20f0a44579e4fc0f300
Nabilleb <nabilgreen500@gmail.com>
2025-08-18 11:54:42 +0300

    conditions to filter products and search product

--- products.php
+++ products.php
@@ -4,4 +4,13 @@ include "connect.php";
 $categoryID = isset($_GET['category']) ? intval($_GET['category']) : 0;
 $keyword = isset($_GET['search']) ? $_GET['search'] : "";
 
+$sql = "SELECT * FROM Products WHERE IsActive = 1";
+if ($categoryID > 0){
+    $sql .= "AND CategoryID = $categoryID";
+}
+
+if (!empty($keyword)){
+        $keywordEscaped = $conn->real_escape_string($keyword);
+        $sql .= " AND (ProductName LIKE '%$keywordEscaped%' OR Description LIKE '%$keywordEscaped%')"
+}
 ?>
